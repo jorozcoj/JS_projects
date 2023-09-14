695a0486d8664732df31252f17cf0085b0c6ca17
jhorman Orozco <j.orozco.j23@gmail.com>
2023-09-14 15:36:15 -0500

    peticiones put y delete

--- servidorWeb/index.js
+++ servidorWeb/index.js
@@ -14,24 +14,53 @@ app.get("/", (req, res) => {
   });
 });
 
+//Obtiene un solo usuario
 app.get("/:id", (req, res) => {
-  let {params:{id}}=req
-  let user=Service.getUser(id)
+  let {
+    params: { id },
+  } = req;
+  let user = Service.getUser(id);
   res.json({
     messaje: `Usuario ${id}`,
     body: user,
   });
 });
 
+/* app.put("/:id", (req, res) => {
+  const user = Service.getUser(req.params.id);
+  let {params: { id }} = req;
+  let { body: newUpdater } = req;
+  if (user.length == 0) {
+    res.status(404).send(`Usuario con id ${req.params.id} no existe`);
+  } else {
+    const result = Service.validatos(req.body);
+    if (result.error) {
+      res.status(400).send(result.error.details[0].message);
+    } else {
+      res.send(Service.updateUser(id, newUpdater));
+    }
+  }
+}); */
+
+//Elimina un usuario
+app.delete("/:id", (req, res) => {
+  let identifier = req.params.id;
+  let deletedUser =Service.deleteUser(identifier);
+
+  res.json({
+    messaje: `usuario ${identifier} eliminado`,
+    body: deletedUser
+  });
+});
 
 //crea un nuevo usuario
 app.post("/", (req, res) => {
-  let {body :newUser} = req;  
+  let { body: newUser } = req;
   let user = Service.createUsers(newUser);
   res.status(201).json({
-    messaje:'Usuario creado',
-    body: user   
-  })
+    messaje: "Usuario creado",
+    body: user,
+  });
 });
 
 app.listen(port, () => {
--- servidorWeb/src/service.js
+++ servidorWeb/src/service.js
@@ -7,13 +7,35 @@ module.exports = {
     let user = data.filter((person) => person.id === identifier)[0];
     return user;
   },
+  modifyUser: (id) => {
+   
+  },
   createUsers: (dataUser) => {
     let newUser = {
       id: data.length + 1,
       ...dataUser,
     };
     data.push(newUser);
-
     return newUser;
+  },  
+
+/*  updateUser: (identificador, updateData) => {
+    let id = Number(identificador);
+    let usuarioBuscado = data.find((cliente) => {
+      return cliente.id === id;
+      
+    });
+    usuarioBuscado.first_name = updateData.first_name;
+    usuarioBuscado.last_name = updateData.last_name;
+    usuarioBuscado.email = updateData.email;
+    return usuarioBuscado;
+  }, */
+
+  deleteUser: (identifier) => {
+    let id =Number(identifier)
+    let deletedUser = data.find((user)=> user.id==identifier)
+    let index = data.findIndex((user)=>user.id===id)
+    data.splice(index,1);
+    return deletedUser
   },
 };
